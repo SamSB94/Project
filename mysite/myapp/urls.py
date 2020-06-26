3e81a215a47a3156bc36a24cd5bfc1575e0f08d1
SamSB94 <soumyasubhra.bhowmik@gmail.com>
2020-06-26 19:00:52 +0530

    26/6/20

--- mysite/myapp/urls.py
+++ mysite/myapp/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
-    path('another.html', views.another)
+    path('', views.index, name='home'),
+    path('another', views.another, name='how to'),
+    path('why', views.why, name='why'),
+    path('donate', views.donate, name='donate')
 ]
--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -7,3 +7,11 @@ def index(request):
 
 def another(request):
     return render(request, 'myapp/another.html')
+
+
+def why(request):
+    return render(request, 'myapp/why.html')
+
+
+def donate(request):
+    return render(request, 'myapp/donate.html')
